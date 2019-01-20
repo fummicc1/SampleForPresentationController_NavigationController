514ff8a8e3f2e1f229472a6dc89e7a5299a6bfd0
fummicc1 <fumiya989@gmail.com>
2019-01-20 11:40:10 +0900

    sample

--- SampleProject_PresentationController/ModalViewController.swift
+++ SampleProject_PresentationController/ModalViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ModalViewController.swift
 //  SampleProject_PresentationController
 //
 //  Created by Fumiya Tanaka on 2019/01/20.
@@ -8,32 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
+class ModalViewController: UIViewController {
 
-    let vc = NavigationController()
+    let vc = ModalSecondViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .green
         setSegueButton()
     }
-
+    
     func setSegueButton() {
         let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 200)))
         button.center = view.center
         button.setTitle("モーダルビュー", for: .normal)
         button.setAttributedTitle(NSAttributedString(string: "モーダルビュー", attributes: [NSAttributedString.Key.foregroundColor: UIColor.blue]), for: UIControl.State.normal)
-        button.addTarget(self, action: #selector(presentVC), for: UIControl.Event.touchUpInside)
+        button.addTarget(self, action: #selector(pushVC), for: UIControl.Event.touchUpInside)
         view.addSubview(button)
     }
     
-    @objc func presentVC() {
-        vc.viewControllers = [ModalViewController()]
-        present(vc, animated: true, completion: nil)
+    @objc func pushVC() {
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        return PresentationController(presentedViewController: presented, presenting: presenting)
-    }
-
 }
---
+++ SampleProject_PresentationController/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  SampleProject_PresentationController
+//
+//  Created by Fumiya Tanaka on 2019/01/20.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
